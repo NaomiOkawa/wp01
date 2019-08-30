c34e1825f8da5d9394e3a014ab9e9e878f77a080
zd3G04 <zd3g04@gmail.com>
2019-08-30 11:42:13 +0900

    title修正3

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
       <?php if (have_posts()): ?>
       <?php while (have_posts()): the_post(); ?>
        <article class="post">
-         <h2>さしすせそ
+         <h2>
           <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
          </h2>
          <p class="postmeta">
