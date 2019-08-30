bafc08c4c3cc418e98415d2929a7ebd7259f184f
zd3G04 <zd3g04@gmail.com>
2019-08-30 11:25:08 +0900

    first commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+</div><!-- content -->
+ <aside>
+      <div class="widget widget_search">
+        <?php get_search_form(); ?>
+      </div>
+      <?php dynamic_sidebar(); ?>
+    </aside>
+  </main>
+  <footer>
+     <div id="link">
+         <?php previous_posts_link(); ?>
+         <?php next_posts_link(); ?>
+    </div>
+  </footer>
+</div><!-- contents -->
+<?php wp_footer(); ?>
+</body>
+</html>
